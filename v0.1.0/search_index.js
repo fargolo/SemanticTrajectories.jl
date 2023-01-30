08941dadfa00179984cddfd41c7161fb9cc5574a
Documenter.jl <documenter@juliadocs.github.io>
2023-01-30 19:35:40 +0000

    build based on 2187fab

---
+++ v0.1.0/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SemanticTrajectories","category":"page"},{"location":"#SemanticTrajectories","page":"Home","title":"SemanticTrajectories","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SemanticTrajectories.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SemanticTrajectories]","category":"page"}]
+}
